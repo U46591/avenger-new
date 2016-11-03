5ab3800e07ddf0ced906327af7a944104a375cf8
induvnair01 <U26436@G021DTRV.ustr.com>
2016-11-03 17:42:32 +0530

    Committing Changes..

--- walmarthack-impl/src/main/webapp/js/app-controller/login.controller.js
+++ walmarthack-impl/src/main/webapp/js/app-controller/login.controller.js
@@ -1,52 +1,52 @@
 /**
  * 
  */
-(function () {
-    'use strict';
+(function() {
+	'use strict';
 
-    angular
-        .module('youngMindApp')
-        .controller('LoginController', LoginController)
-        .directive('passwordValidate',PasswordValidate);
+	angular.module('youngMindApp').controller('LoginController',
+			LoginController).directive('passwordValidate', PasswordValidate);
 
-    LoginController.$inject = ['$location'];
-    function LoginController($location) {
-       var ym=this;
-       ym.user={};
-       ym.login=Login;
-       
-       var staticMethods={};
-       staticMethods.logout=Logout;
-       
-       return staticMethods;
-       
-        (function initController() {
-         // alert('login controller ----');
-        })();
-            
-        function Logout(){
-        //	alert('logout ------');
-        };
-        
-        function Login() {
-        //	alert('login ------');
-        	UserService.POST('login',ym.user)
-            .then(function (response) {
-                if (response.success) {
-                	AuthenticationService.SetCredentials(response.data);
-                    $location.path('/successPage');
-                } else {
-                    //FlashService.Error(response.message);
-                    //vm.dataLoading = false;
-                	alert('You Have Entered a Wrong Credential');
-                }
-            });
-        };
-    
-    }
-    
-    function PasswordValidate(){
-    	//alert('password validate')
-    }
+	LoginController.$inject = [ '$location','UserService'];
+	function LoginController($location,UserService) {
+		var ym = this;
+		ym.user = {};
+		ym.login = Login;
+		ym.user.name="";
+		ym.user.name="";
+		ym.user.mobileNo="";
+		
+		
+
+		
+
+		(function initController() {
+			// alert('login controller ----');
+		})();
+
+		function Logout() {
+			// alert('logout ------');
+		}
+		;
+		function Login() {
+			// alert('login ------');
+			UserService.POST('login', ym.user).then(function(response) {
+				if (response.success) {
+					AuthenticationService.SetCredentials(response.data);
+					$location.path('/successPage');
+				} else {
+					// FlashService.Error(response.message);
+					// vm.dataLoading = false;
+					alert('You Have Entered a Wrong Credential');
+				}
+			});
+		}
+		;
+
+	}
+
+	function PasswordValidate() {
+		// alert('password validate')
+	}
 
 })();
