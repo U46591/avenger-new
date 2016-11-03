a9929ed879d095089430875e2b7a5ec8b9cfadcd
Gafoor Palakkal <gafoordosthg@gmail.com>
2016-11-03 13:31:40 +0530

    connecting login with service layer

--- walmarthack-impl/src/main/java/com/youngminds/dao/impl/UserDaoImpl.java
+++ walmarthack-impl/src/main/java/com/youngminds/dao/impl/UserDaoImpl.java
@@ -4,9 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -36,7 +38,8 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public User validateUser(User user) {
-		User resultUser = null;
+		User resultUser=null;
+		/*User resultUser = null;
 		Object params[] = new Object[] { user.getMobileNo().toString(), user.getPassword() };
 
 		String sql = "SELECT * FROM users WHERE mobile_no=? and password=?";
@@ -45,8 +48,11 @@ public class UserDaoImpl implements UserDao {
 			// params);
 		} catch (Exception exp) {
 			System.out.println(exp);
-		}
-
+		}*/
+		Criteria criteria=sessionFactory.openSession().createCriteria(User.class);
+		criteria.add(Restrictions.eq("mobileNo", user.getMobileNo()))
+		.add(Restrictions.eq("password", user.getPassword()));
+		resultUser=(User) criteria.uniqueResult();
 		return resultUser;
 
 	}
